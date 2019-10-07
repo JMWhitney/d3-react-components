3ad368ac0287e9d97eba2fc0a6386432810ac8a8
JMWhitney <JMWhitney96@gmail.com>
2019-10-06 23:28:26 -0700

    Added utility function to make charts responsive/resizable.

--- src/BarChart.js
+++ src/BarChart.js
@@ -7,12 +7,23 @@ import { timeDay } from 'd3-time';
 import * as ease from 'd3-ease';
 import { transition } from 'd3-transition';
 import { zoom } from 'd3-zoom';
+import { brushX } from 'd3-brush';
 import propTypes from 'prop-types';
+import responsify from './util/responsify';
 
 const drawChart = (d, _id) => {
-  var margin = { top: 0, right: 0, bottom: 30, left: 20 },
-      height = 400 - margin.top - margin.bottom,
-      width = 600 - margin.left - margin.right,
+
+  try {
+    selectAll(`#${_id} g`).remove();
+    select(`#chart_tooltip`).remove()
+  } catch (error) {
+    console.error(error);
+  }
+
+  var svg = select(`#${_id}`).call(responsify),
+      margin = { top: 0, right: 0, bottom: 30, left: 20 },
+      height = svg.node().getBoundingClientRect().height - margin.top - margin.bottom,
+      width = svg.node().getBoundingClientRect().width - margin.left - margin.right,
       tempColor,
       yScale,
       yAxisValues,
@@ -24,54 +35,55 @@ const drawChart = (d, _id) => {
       xGuide,
       colors,
       tooltip,
+      brush,
       myChart;
 
   yScale = scaleLinear()
-    .domain([0, max(d.temperatures)])
+    .domain([0, max(d.y)])
     .range([0,height]);
 
   yAxisValues = scaleLinear()
-    .domain([0, max(d.temperatures)])
+    .domain([0, max(d.y)])
     .range([height, 0])
   
   yAxisTicks = axisLeft(yAxisValues)
     .ticks(10);
   
   xScale = scaleBand()
-    .domain(d.temperatures)
+    .domain(d.y)
     .paddingInner(.1)
     .paddingOuter(.1)
     .range([0, width]);
 
   xAxisValues = scaleTime()
-    .domain([ d.dates[0], d.dates[d.dates.length - 1] ])
+    .domain([ d.x[0], d.x[d.x.length - 1] ])
     .range([0, width]);
 
   xAxisTicks = axisBottom(xAxisValues)
-    .ticks(timeDay.every(parseInt(d.dates.length / 5)));
+    .ticks(timeDay.every(parseInt(d.x.length / 5)));
   
   colors = scaleLinear()
-    .domain([ 0, 60, max(d.temperatures) ])
+    .domain([ 0, 60, max(d.y) ])
     .range(['#FFF', '#2D8BCF', '#DA3637']);
   
-  tooltip = select('body')
-              .append('div')
-              .style('position', 'absolute')
-              .style('padding', '0 10px')
-              .style('background', 'white')
-              .style('opacity', 0);
-  
-  select(`#${_id} svg`).remove(`#{_id} svg`);
+  tooltip = 
+  select('body')
+    .append('div')
+    .attr('id', 'chart_tooltip')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0);
 
   myChart = 
-  select(`#${_id}`).append('svg')
+  svg.append('g')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', 
             'translate(' + margin.left + ',' + margin.right + ')'
           )
-  .selectAll('rect').data(d.temperatures)
+  .selectAll('rect').data(d.y)
     .enter().append('rect')
       .attr('fill', colors)
       .attr('width', function(d) {
@@ -102,13 +114,17 @@ const drawChart = (d, _id) => {
           .style('fill', tempColor)
       });
 
-  yGuide = select(`#${_id} svg`).append('g')
+  yGuide = svg.append('g')
     .attr('transform', 'translate(20, 0)')
     .call(yAxisTicks);
 
-  xGuide = select(`#${_id} svg`).append('g')
+  xGuide = svg.append('g')
     .attr('transform', 'translate(20,' + height + ')')
     .call(xAxisTicks);
+
+  brush = svg.append('g')
+    .attr("class", "brush")
+    .call(brushX().on("brush", () => console.log("Brushy brushy")))
   
   myChart.transition()
     .attr('height', function(d) {
@@ -131,17 +147,14 @@ function BarChart(props) {
     drawChart(props.data, props._id);
   })
 
-  return (
-    <div id={props._id} className="BarChart">
-
-    </div>
-  );
+  return <svg id={props._id} width="100%" height="100%"></svg>
+  
 }
 
 BarChart.propTypes = {
   data: propTypes.shape({
-    temperatures: propTypes.array.isRequired,
-    dates: propTypes.array.isRequired,
+    y: propTypes.array.isRequired,
+    x: propTypes.array.isRequired,
   }),
   _id: propTypes.string.isRequired
 }
--- src/index.js
+++ src/index.js
@@ -1,53 +1,40 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import BarChart from './BarChart';
+// import LineChart from './LineChart';
+// import PracticeChart from './PracticeChart';
+
+const randomValues = () => {
+  return {
+    y: (() => {
+      let y = []
+      y.push(50 + Math.random() * 40)
+      for (var i = 1; i < 50; i++) {
+        y.push( y[i-1] + (Math.random() - 0.5) * 10  )
+      }
+      return y
+    })(),
+    x: (() => {
+      let x = [];
+      x.push(new Date())
+      for (var i = 1; i < 50; i++) {
+        x.push( new Date( x[i-1].getTime() + (24 * 60 * 60 * 1000)) )
+      }
+      return x;
+    })(),
+  }
+}
 
 const Layout = () => {
   // Dummy data to render with charts
-  const [data, setdata] = useState({
-    temperatures: (() => {
-      let temperatures = []
-      temperatures.push(50 + Math.random() * 40)
-      for (var i = 1; i < 50; i++) {
-        temperatures.push( temperatures[i-1] + (Math.random() - 0.5) * 10  )
-      }
-      return temperatures
-    })(),
-    dates: (() => {
-      let dates = [];
-      dates.push(new Date)
-      for (var i = 1; i < 50; i++) {
-        dates.push( new Date( dates[i-1].getTime() + (24 * 60 * 60 * 1000)) )
-      }
-      return dates;
-    })(),
-  })
-
-  const randomValues = () => {
-    setdata({
-      temperatures: (() => {
-        let temperatures = []
-        temperatures.push(50 + Math.random() * 40)
-        for (var i = 1; i < 50; i++) {
-          temperatures.push( temperatures[i-1] + (Math.random() - 0.5) * 10  )
-        }
-        return temperatures
-      })(),
-      dates: (() => {
-        let dates = [];
-        dates.push(new Date)
-        for (var i = 1; i < 50; i++) {
-          dates.push( new Date( dates[i-1].getTime() + (24 * 60 * 60 * 1000)) )
-        }
-        return dates;
-      })(),
-    })
-  }
+  const [data, setData] = useState(randomValues())
 
   return ( 
     <div id="charts">
-      <BarChart _id={"a"} data={data}/>
-      <button onClick={randomValues}>New Values</button>
+      <div style={{display: "flex"}}>
+        <BarChart data={data} _id={"c"}/>
+      </div>
+      <button onClick={() => setData(randomValues)}>New Values</button> 
     </div> 
   );
 }
